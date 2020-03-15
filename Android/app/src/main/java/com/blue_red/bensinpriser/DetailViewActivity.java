377040b9c10be469ddbc5a978270a10502615d96
JonathanHoffmann <jonathan.hoffmann@gmx.net>
2020-03-15 13:47:42 +0100

    New Activity on Clicking on item in RecyclerView

---
+++ Android/app/src/main/java/com/blue_red/bensinpriser/DetailViewActivity.java
@@ -0,0 +1,14 @@
+package com.blue_red.bensinpriser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
--- Android/app/src/main/java/com/blue_red/bensinpriser/FuelStationAdapter.java
+++ Android/app/src/main/java/com/blue_red/bensinpriser/FuelStationAdapter.java
@@ -1,5 +1,6 @@
 package com.blue_red.bensinpriser;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -23,6 +24,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 
 public class FuelStationAdapter extends RecyclerView.Adapter<BaseViewHolder> {
@@ -36,7 +38,7 @@ public class FuelStationAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
     public FuelStationAdapter(String fuel, List<FuelStation> fuelStationList) {
         mFuelStationList = fuelStationList;
-        this.fuel=fuel;
+        this.fuel = fuel;
     }
 
     public String getFuel() {
@@ -132,25 +134,16 @@ public class FuelStationAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
             final FuelStation mFuelStation = mFuelStationList.get(position);
             double pricetemp;
-            if (fuel.equals("Bensin 95"))
-            {
-                pricetemp=mFuelStation.getmBensin95();
-            }
-            else if (fuel.equals("Bensin 98"))
-            {
-                pricetemp=mFuelStation.getmBensin98();
-            }
-            else if (fuel.equals("Diesel"))
-            {
-                pricetemp=mFuelStation.getmDiesel();
-            }
-            else if (fuel.equals("Ethanol 85"))
-            {
-                pricetemp=mFuelStation.getmEthanol85();
-            }
-            else
-            {
-                pricetemp=-1;
+            if (fuel.equals("Bensin 95")) {
+                pricetemp = mFuelStation.getmBensin95();
+            } else if (fuel.equals("Bensin 98")) {
+                pricetemp = mFuelStation.getmBensin98();
+            } else if (fuel.equals("Diesel")) {
+                pricetemp = mFuelStation.getmDiesel();
+            } else if (fuel.equals("Ethanol 85")) {
+                pricetemp = mFuelStation.getmEthanol85();
+            } else {
+                pricetemp = -1;
             }
 
             if (mFuelStation.getImageUrl() != null) {
@@ -162,12 +155,12 @@ public class FuelStationAdapter extends RecyclerView.Adapter<BaseViewHolder> {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        if (coverImageView.getDrawable() == null){
+                        if (coverImageView.getDrawable() == null) {
                             //Image doesn´t exist.
                             companyNameTextView.setVisibility(View.VISIBLE);
                             companyNameTextView.setText(mFuelStation.getmCompanyName());
                             coverImageView.setVisibility(View.INVISIBLE);
-                        }else{
+                        } else {
                             //Image Exists!.
                             companyNameTextView.setVisibility(View.GONE);
                         }
@@ -186,45 +179,41 @@ public class FuelStationAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
             priceTextView.setText(fuel + " pris: " + pricetemp + "kr");
 
+
             itemView.setOnClickListener(v -> {
-                if (mFuelStation.getImageUrl() != null) {
-                    try {
-                        Intent intent = new Intent();
-                        intent.setAction(Intent.ACTION_VIEW);
-                        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                        intent.setData(Uri.parse(mFuelStation.getImageUrl()));
-                        itemView.getContext().startActivity(intent);
-                    } catch (Exception e) {
-                        Log.e(TAG, "onClick: Image url is not correct");
-                    }
-                }
-            });
-        }
+                //https://stackoverflow.com/questions/20241857/android-intent-cannot-resolve-constructor/20241921
+                Intent intent = new Intent(v.getContext(), DetailViewActivity.class);
+                //https://stackoverflow.com/questions/33173043/cannot-resolve-method-startactivity
+                v.getContext().startActivity(intent);
+
+            }
+        );
+    }
+}
+
+public class EmptyViewHolder extends BaseViewHolder {
+
+    @BindView(R.id.tv_message)
+    TextView messageTextView;
+    @BindView(R.id.buttonRetry)
+    TextView buttonRetry;
+
+    EmptyViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+        buttonRetry.setOnClickListener(v -> {
+            try {
+                mCallback.onEmptyViewRetryClick();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    public class EmptyViewHolder extends BaseViewHolder {
-
-        @BindView(R.id.tv_message)
-        TextView messageTextView;
-        @BindView(R.id.buttonRetry)
-        TextView buttonRetry;
-
-        EmptyViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-            buttonRetry.setOnClickListener(v -> {
-                try {
-                    mCallback.onEmptyViewRetryClick();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-        @Override
-        protected void clear() {
-
-        }
+    @Override
+    protected void clear() {
 
     }
+}
+
 }
