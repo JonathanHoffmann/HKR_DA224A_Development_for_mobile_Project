5d7e000c19534b9fa291724ef62b80b0c74337cd
Sergheev Andrian <sergheev@icloud.com>
2020-03-17 08:58:30 +0100

    detail view with prices

--- Android/app/src/main/java/com/blue_red/bensinpriser/detail/DetailViewActivity.kt
+++ Android/app/src/main/java/com/blue_red/bensinpriser/detail/DetailViewActivity.kt
@@ -13,6 +13,13 @@ class DetailViewActivity : AppCompatActivity() {
     private var fuelStation: FuelStation? = null
 
     private lateinit var imageView: ImageView
+    private lateinit var titleTextView: TextView
+    private lateinit var subtitleTextView: TextView
+
+    private lateinit var bensin98TextView: TextView
+    private lateinit var bensin95TextView: TextView
+    private lateinit var dieselTextView: TextView
+    private lateinit var ethanol85TextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,6 @@ class DetailViewActivity : AppCompatActivity() {
 
     private fun setupComponents() {
         imageView = findViewById(R.id.logoImageView)
-
         Glide.with(this).load(fuelStation?.imageUrl).into(imageView)
     }
 
@@ -32,10 +38,33 @@ class DetailViewActivity : AppCompatActivity() {
         val intent = this.intent
         val bundle = intent.extras
         fuelStation = bundle?.getSerializable("Station") as FuelStation?
-        val titleText = findViewById<TextView>(R.id.TVtitle)
-        titleText.text = fuelStation?.title
 
-        val subtitleText = findViewById<TextView>(R.id.companyName)
-        subtitleText.text = fuelStation?.getmCompanyName()
+        titleTextView = findViewById(R.id.TVtitle)
+        titleTextView.text = fuelStation?.title
+
+        subtitleTextView = findViewById(R.id.companyName)
+        subtitleTextView.text = fuelStation?.getmCompanyName()
+
+        bensin95TextView = findViewById(R.id.textView11)
+        bensin95TextView.text = "Bensin95 price: " + formatForNull(fuelStation?.getmBensin95())
+
+        bensin98TextView = findViewById(R.id.textView18)
+        bensin98TextView.text = "Bensin98 price: " + formatForNull(fuelStation?.getmBensin98())
+
+        dieselTextView = findViewById(R.id.textView12)
+        dieselTextView.text = "Diesel price: " + fuelStation?.getmDiesel() + "SEK"
+
+        ethanol85TextView = findViewById(R.id.textView13)
+        ethanol85TextView.text = "Ethanol price: " + fuelStation?.getmEthanol85() + "SEK"
+    }
+
+    private fun formatForNull(price: Double?): String {
+        if (price != null) {
+            if (price == -1.0) {
+                return "Not Available"
+            }
+            return price.toString() + " SEK"
+        }
+        return "Not Available"
     }
 }
